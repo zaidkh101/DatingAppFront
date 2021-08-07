53e44ea12694d32b7e396ef957b3b8922a755f7f
Zaid Al-Khader <zeidkh1998@gmail.com>
2021-08-07 20:59:59 +0300

    Adding Routing | Zaid

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -15,7 +17,7 @@ export class NavComponent implements OnInit {
   // currentUser$: Observable<User>
 
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     // this.currentUser$ = this.accountService.currentUser$;
@@ -23,10 +25,10 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
-
+      this.router.navigateByUrl("/members");
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     }
 
     )
@@ -34,6 +36,7 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl("/");
 
   }
 
