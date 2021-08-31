d0ce55ad11601889bcf0e34a71ce6e2428700308
Zaid Al-Khader <zeidkh1998@gmail.com>
2021-08-31 23:49:22 +0300

    Added SignalR with Presence Hub Services

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -4,6 +4,7 @@ import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
+import { PresenceService } from './presence.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,7 @@ export class AccountService {
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private presence: PresenceService) { }
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'account/login', model).pipe(
@@ -22,6 +23,7 @@ export class AccountService {
         const user = response
         if (user) {
           this.setCurrentUser(user);
+          this.presence.createHubConnection(user);
         }
 
       })
@@ -39,6 +41,7 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+    this.presence.stopHubConnection();
   }
 
   register(model: any) {
@@ -47,6 +50,7 @@ export class AccountService {
         if (user) {
 
           this.setCurrentUser(user);
+          this.presence.createHubConnection(user);
         }
 
       }))
